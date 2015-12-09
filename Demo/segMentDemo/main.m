3f3fdf64c7e8ddd0c2cc97ef3d7241efa482dc54
mandy04 <ma_chun_hui@163.com>
2015-12-10 04:40:55 +0800

    addFile

---
+++ Demo/segMentDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  segMentDemo
+//
+//  Created by llbt on 15/11/30.
+//  Copyright © 2015年 llbt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
